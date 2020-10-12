a2473d28dc7cfe0c92268dba65bad919595989ee
Byko <bykocrypto@gmail.com>
2020-10-13 01:07:49 +0200

    📒 functions add to wallet.sol
    - withdraw function
    - balanceOf function
    - spend function 
    - get price function
    - save function => add yDai to savings

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.6.0;
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 interface IYDAI {
     function deposit(uint _amount)  external;
-    function withdraw(uint, _shares) external;
-    function balanceOf(address account) public view returns (uint);
-    function getPricePerFullShare() public view returns (uint);
+    function withdraw(uint _shares) external;
+    function balanceOf(address account) external view returns (uint);
+    function getPricePerFullShare() external view returns (uint);
 }
 
 contract Wallet {
     address admin;
-    IERC20 dai = IERC20(0x6b175474e89094c44da98b954eedeac495271d0f);
+    IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     IYDAI yDai = IYDAI(0xC2cB1040220768554cf699b0d863A3cd4324ce32);
 
     constructor() public {
@@ -19,8 +19,29 @@ contract Wallet {
 
     function save(uint amount) external {
         dai.transferFrom(msg.sender, address(this), amount);
+        _save(amount);
+    }
+
+    function spend(uint amount, address recipient) external {
+        require(msg.sender == admin, 'only admin');
+        uint balanceShares = yDai.balanceOf(address(this));
+        yDai.withdraw(balanceShares);
+        dai.transfer(recipient, amount);
+        uint balanceDai = dai.balanceOf(address(this));
+        if(balanceDai > 0) {
+             _save(balanceDai);
+        }
+    }
+
+    function _save(uint amount) internal {
         dai.approve(address(yDai), amount);
-        yDai.deposit(amount);
+             yDai.deposit(amount);
+    }
+
+    function balance() external view returns(uint) {
+        uint price = yDai.getPricePerFullShare();
+        uint balanceShares = yDai.balanceOf(address(this));
+        return balanceShares * price;
     }
 
 }
